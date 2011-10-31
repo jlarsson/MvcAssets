b13c69b93090a445d7020548b4a392915adbc86f
Joakim Larsson <mimmertim@gmail.com>
2011-10-31 21:25:03 +0100

    added psake scripts for building nuget packages tagged version 1.2

--- WebAssets/Properties/AssemblyInfo.cs
+++ WebAssets/Properties/AssemblyInfo.cs
@@ -1,39 +1,11 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("WebAssets")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("WebAssets")]
-[assembly: AssemblyCopyright("Copyright © HP 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("030c631d-bfdd-4532-933d-a53dd7a9bdad")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliantAttribute(true)]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute("WebAssets ")]
+[assembly: AssemblyDescriptionAttribute("Asset manager")]
+[assembly: AssemblyCompanyAttribute("Joakim Larsson")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Joakim Larsson 2011")]
+[assembly: AssemblyVersionAttribute("1.0.*")]
+[assembly: AssemblyDelaySignAttribute(false)]
